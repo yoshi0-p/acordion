ad11aa28364481ad1be56bf0e21fae0ae5a0d411
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-12 16:06:27 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <div class="gray flame">
+    <p v-on:click= "acr" >アコーディオン</p>
+  </div>
+  <div class= "white flame" v-if = "acr">
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.flame{
+  height:50px;
+  width:400px;
+}
+.gray{
+  background-color:gray;
+}
+</style>
